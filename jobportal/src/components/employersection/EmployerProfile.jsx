4b769c4e9f77260f9df3f74e7b922d2d60a56bd4
ishanupadhyay9 <ishanupadhyay1000@gmail.com>
2025-09-02 01:08:18 +0530

    Update EmployerProfile.jsx

--- jobportal/src/components/employersection/EmployerProfile.jsx
+++ jobportal/src/components/employersection/EmployerProfile.jsx
@@ -76,7 +76,7 @@ export default function EmployerProfileForm() {
   };
 
   return (
-    <div className="min-h-screen w-full  flex items-center justify-center p-8">
+    <div className="min-h-screen w-4xl  flex items-center justify-center p-8">
       <form
         className="w-full  bg-white ml-20 rounded-2xl shadow-2xl p-10 space-y-7 border border-gray-100"
         onSubmit={handleSubmit}
