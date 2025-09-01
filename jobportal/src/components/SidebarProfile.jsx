b8e28febe7ec320a63965ea865a5295910d78982
ishanupadhyay9 <ishanupadhyay1000@gmail.com>
2025-09-01 22:59:28 +0530

    Update SidebarProfile.jsx

--- jobportal/src/components/SidebarProfile.jsx
+++ jobportal/src/components/SidebarProfile.jsx
@@ -1,49 +1,53 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router';
+
 const SidebarProfile = (props) => {
   const showProfile = props.showprofile;
   const setShowProfile = props.setShowProfile;
-  const isregistered = useSelector((state)=>state.auth.isRegistered);
-  return (
-    <div>
-       <aside className="w-64 h-[1000px] sticky bg-gray-200 border-r border-base-300 hidden lg:flex flex-col bottom-5 right-6 " data-theme="light">
-    
+  const isregistered = useSelector((state) => state.auth.isRegistered);
 
-      <nav className="flex-1 p-4 space-y-1">
-       {isregistered && <Link
-          to="/"
-          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case  `}
-        >
-          <span>Home</span>
-        </Link>} 
+  return (
+    <aside
+      className="w-64 h-screen fixed top-0 left-0 bg-gray-200 border-r border-base-300 hidden lg:flex flex-col"
+      data-theme="light"
+    >
+      <nav className="flex-1 p-4 space-y-1 mt-4">
+        {isregistered && (
+          <Link
+            to="/"
+            className="btn btn-ghost justify-start w-full gap-3 px-3 normal-case"
+          >
+            <span>Home</span>
+          </Link>
+        )}
 
         <button
-   
-          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case rounded-xl
-         ${showProfile&&"bg-gray-600 text-white"}`}
-         onClick={()=>{setShowProfile(true)}}
+          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case rounded-xl ${
+            showProfile && "bg-gray-600 text-white"
+          }`}
+          onClick={() => {
+            setShowProfile(true);
+          }}
         >
           <span>My Profile</span>
         </button>
 
-       {
-        isregistered &&  <button
-          className={`btn btn-ghost justify-start w-full gap-3 px-3 rounded-xl normal-case 
-          ${!showProfile&&"bg-gray-600 text-white"} `}
-                   onClick={()=>{setShowProfile(false)}}
-
-        >
-          <span>Applications</span>
-        </button>
-       }
+        {isregistered && (
+          <button
+            className={`btn btn-ghost justify-start w-full gap-3 px-3 rounded-xl normal-case ${
+              !showProfile && "bg-gray-600 text-white"
+            }`}
+            onClick={() => {
+              setShowProfile(false);
+            }}
+          >
+            <span>Applications</span>
+          </button>
+        )}
       </nav>
-
-     
-      
     </aside>
-    </div>
-  )
-}
+  );
+};
 
-export default SidebarProfile
+export default SidebarProfile;
